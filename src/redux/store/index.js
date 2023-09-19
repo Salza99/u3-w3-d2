c874689c8815a4cf5d394ea69bfdb145a05ec48c
Salza99 <137687037+Salza99@users.noreply.github.com>
2023-09-19 17:00:25 +0200

    add encrypt-transform

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,6 +1,7 @@
 import { combineReducers, configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
 import { encryptTransform } from "redux-persist-transform-encrypt";
+
 import storage from "redux-persist/lib/storage";
 import favoritesReducer from "../reducers/favoritesReducer";
 import jobsReducer from "../reducers/jobsReducer";
